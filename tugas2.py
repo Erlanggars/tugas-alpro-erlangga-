248ba5f675ab05fd101bc1be9882b24f588eb72a
Erlanggars <151703086+Erlanggars@users.noreply.github.com>
2023-11-28 18:04:31 -0800

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,12 @@
+def susun_bilangan():
+    bilangan = [23, 45, 76, 24, 56, 98, 69, 16, 54]
+    return bilangan
+
+# Ascending(Terkecil ke terbesar)
+bilangan = susun_bilangan()
+bilangan_terurut = sorted(bilangan)
+print("Bilangan setelah disusun secara ascending:", bilangan_terurut)
+
+# Descending (Terbesar ke terkecil)
+bilangan_terurut = sorted(bilangan, reverse=True)
+print("Bilangan setelah disusun secara descending:", bilangan_terurut)
